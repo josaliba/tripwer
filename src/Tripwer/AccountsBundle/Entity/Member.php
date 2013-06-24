2211b15abe58a8b9f229f930e188a3d17428f8e6
josephsaliba <joseph@coddict.com>
2013-06-24 19:28:32 -0400

    created member entity

---
+++ src/Tripwer/AccountsBundle/Entity/Member.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Tripwer\AccountsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User;
+
+/**
+ * Member
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity
+ */
+class Member extends User
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sex", type="string", length=255)
+     */
+    private $sex;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="dateOfBirth", type="datetime")
+     */
+    private $dateOfBirth;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set sex
+     *
+     * @param string $sex
+     * @return Member
+     */
+    public function setSex($sex)
+    {
+        $this->sex = $sex;
+    
+        return $this;
+    }
+
+    /**
+     * Get sex
+     *
+     * @return string 
+     */
+    public function getSex()
+    {
+        return $this->sex;
+    }
+
+    /**
+     * Set dateOfBirth
+     *
+     * @param string $dateOfBirth
+     * @return Member
+     */
+    public function setDateOfBirth($dateOfBirth)
+    {
+        $this->dateOfBirth = $dateOfBirth;
+    
+        return $this;
+    }
+
+    /**
+     * Get dateOfBirth
+     *
+     * @return string 
+     */
+    public function getDateOfBirth()
+    {
+        return $this->dateOfBirth;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return Member
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+}
