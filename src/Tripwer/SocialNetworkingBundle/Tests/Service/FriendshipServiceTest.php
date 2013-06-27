78d249f61a1315b80296443efcf12ce11fff7543
josephsaliba <joseph@coddict.com>
2013-06-27 02:06:19 -0400

    deleted unused test and initiated Friendship service test

---
+++ src/Tripwer/SocialNetworkingBundle/Tests/Service/FriendshipServiceTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tripwer\SocialNetworkingBundle\Tests\Service;
+
+class FriendshipServiceTest extends \PHPUnit_Framework_TestCase{
+
+    public function testCreate(){
+        $this->assertEquals(42, 42);
+    }
+
+}
